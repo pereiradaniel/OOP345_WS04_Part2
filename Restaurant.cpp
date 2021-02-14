e48aeb50fada337586a57bb0490d6ce39554e87b
pereiradaniel <danielpaulpereira@protonmail.com>
2021-02-14 01:07:01 -0500

    Update restaurant

--- Restaurant.cpp
+++ Restaurant.cpp
@@ -1,5 +1,29 @@
 #include "Restaurant.h"
 
 namespace sdds {
+	Restaurant::~Restaurant() {
+		delete[] _res;
+		_res = nullptr;
+	}
 
+	Restaurant::Restaurant(Reservation* reservations[], size_t cnt) {
+	}
+
+	Restaurant& Restaurant::operator=(const Restaurant& src) {
+	}
+
+	Restaurant::Restaurant(const Restaurant& src) {
+	}
+
+	Restaurant &Restaurant::operator=(Restaurant&& src) noexcept {
+	}
+
+	Restaurant::Restaurant(Restaurant&& src) noexcept {
+	}
+
+	size_t Restaurant::size() const {
+	}
+
+	ostream& operator<<(ostream& os, Restaurant& copy) {
+	}
 }
--- Restaurant.h
+++ Restaurant.h
@@ -1,9 +1,26 @@
 #ifndef SDDS_RESTAURANT_H
 #define SDDS_RESTAURANT_H
 
+#include <iostream>
+#include "Reservation.h"
+
 namespace sdds {
 	class Restaurant {
+		Reservation* res_res{ nullptr }; // a dynamically allocated array of objects of type `Reservation`: `Reservation*`
+		size_t res_num;
+	public:
+		Restaurant();
+		Restaurant(Reservation* reservations[], size_t cnt); // a constructor that receives as a parameter an array of pointers to objects of type `Reservation`
+		
+		Restaurant(const Restaurant& src);
+		Restaurant& operator=(const Restaurant& src);
+		Restaurant(Restaurant&& src);
+		Restaurant& operator=(Restaurant&& src);
+		
+		size_t size() const; // a query that returns the number of reservations in the system.
+		~Restaurant();
 
+		friend ostream& operator<<(ostream& os, Restaurant& src); // overload the insertion operator to insert the content of a `Restaurant` object into an **ostream** object.
 	};
 }
 
